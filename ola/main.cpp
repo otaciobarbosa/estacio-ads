98fd3cb2877b4cee2eadda11ee613a22788b170a
Otácio Barbosa <otacio.barbosa@outlook.com>
2023-08-09 07:45:16 -0300

    ola

---
+++ ola/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+string nome;
+cout<<"Qual eh o seu nome?"<<endl;
+cin>>nome;
+cout<<"Oi "<<nome<<", Tudo bem com voce ?";
+return 0;
+
+}
