dc0ab4f4c0a8a03707ad9736c1ff7da4793346ad
Otácio Barbosa <otacio.barbosa@outlook.com>
2023-08-09 07:44:33 -0300

    media

---
+++ media/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	float nota1;
+	float nota2;
+	float nota3;
+	float nota4;
+	float media;
+	
+	nota1 = 9;
+	nota2 = 7.5;
+	nota3 = 4;
+	nota4 = 5;
+	media = (nota1+nota2+nota3+nota4)/4;
+	
+	cout<<"A media do aluno e : "<<media;
+	return 0;
+}
